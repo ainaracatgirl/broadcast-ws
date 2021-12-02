2f584f22b09261902f9e4f469d0372e7a83f8f9e
JanCraft <jancraft888@gmail.com>
2021-12-02 19:29:36 +0100

    small fix: single point of failure crashes

--- app.js
+++ app.js
@@ -9,29 +9,33 @@ wss.on('connection', (ws) => {
     ws.listens = [];
 
     ws.on('message', (data) => {
-        const packet = JSON.parse(data);
-        if (!ws.eventReady) {
-            const { clientID, listens } = packet;
-            ws.clientID = clientID || uuid.v4();
-            ws.listens = listens || [];
-            ws.eventReady = true;
-            return;
-        }
+        try {
+            const packet = JSON.parse(data);
+            if (!ws.eventReady) {
+                const { clientID, listens } = packet;
+                ws.clientID = clientID || uuid.v4();
+                ws.listens = listens || [];
+                ws.eventReady = true;
+                return;
+            }
 
-        packet.$sender = ws.clientID;
+            packet.$sender = ws.clientID;
 
-        if ('$target' in packet) {
-            wss.clients.forEach((client) => {
-                if (client.clientID == packet.$target && client !== ws && client.readyState === WebSocket.OPEN && client.eventReady && client.listens.includes(JSON.parse(data).event)) {
-                    client.send(JSON.stringify(packet));
-                }
-            });
-        } else {
-            wss.clients.forEach((client) => {
-                if (client !== ws && client.readyState === WebSocket.OPEN && client.eventReady && client.listens.includes(JSON.parse(data).event)) {
-                    client.send(JSON.stringify(packet));
-                }
-            });
+            if ('$target' in packet) {
+                wss.clients.forEach((client) => {
+                    if (client.clientID == packet.$target && client !== ws && client.readyState === WebSocket.OPEN && client.eventReady && client.listens.includes(JSON.parse(data).event)) {
+                        client.send(JSON.stringify(packet));
+                    }
+                });
+            } else {
+                wss.clients.forEach((client) => {
+                    if (client !== ws && client.readyState === WebSocket.OPEN && client.eventReady && client.listens.includes(JSON.parse(data).event)) {
+                        client.send(JSON.stringify(packet));
+                    }
+                });
+            }
+        } catch {
+            ws.send(JSON.stringify({ $error: "Incoming packet malformed" }));
         }
     });
 });
